a899b0c0fba9308408f334805e31c49d2a235794
romulogoncalves94 <romulogoncalves94@hotmail.com>
2024-10-15 11:57:56 -0300

    Add documentação OpenAPI

--- user-service-api/src/main/java/br/com/helpdesk/userserviceapi/controller/UserController.java
+++ user-service-api/src/main/java/br/com/helpdesk/userserviceapi/controller/UserController.java
@@ -1,13 +1,39 @@
 package br.com.helpdesk.userserviceapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import models.exceptions.StandardError;
 import models.responses.UserResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "UserController", description = "Controller responsible for user operations")
 @RequestMapping("/api/users")
 public interface UserController {
 
+    @Operation(summary = "Find User By ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User found"),
+            @ApiResponse(
+                    responseCode = "404", description = "User not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = StandardError.class)
+                    )),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = StandardError.class)
+                    ))
+    })
     @GetMapping("/{id}")
-    ResponseEntity<UserResponse> findById(@PathVariable String id);
+    ResponseEntity<UserResponse> findById(
+            @Parameter(description = "User ID", required = true)
+            @PathVariable String id);
 
 }
