ed29246e3962920585b534d5e1167171806523f7
romulogoncalves94 <romulogoncalves94@hotmail.com>
2025-08-07 10:59:27 -0300

    feat: add user entity

---
+++ user-service-api/src/main/java/br/com/helpdesk/userserviceapi/entity/User.java
@@ -0,0 +1,24 @@
+package br.com.helpdesk.userserviceapi.entity;
+
+import lombok.*;
+import models.enums.ProfileEnum;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Set;
+
+@Data
+@With
+@Document
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private Set<ProfileEnum> profile;
+
+}
