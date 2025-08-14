44acc976b2833a7e527cf518a1001999a0bf54d4
romulogoncalves94 <romulogoncalves94@hotmail.com>
2025-08-14 16:38:53 -0300

    feat: add manipulador de exceções para objetos não encontrados.

---
+++ hd-commons-lib/src/main/java/models/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package models.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
--- user-service-api/src/main/java/br/com/helpdesk/userserviceapi/service/UserService.java
+++ user-service-api/src/main/java/br/com/helpdesk/userserviceapi/service/UserService.java
@@ -3,6 +3,7 @@ package br.com.helpdesk.userserviceapi.service;
 import br.com.helpdesk.userserviceapi.mapper.UserMapper;
 import br.com.helpdesk.userserviceapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import models.exceptions.ResourceNotFoundException;
 import models.responses.UserResponse;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,9 @@ public class UserService {
     private final UserMapper mapper;
 
     public UserResponse findById(final String id) {
-        return mapper.fromEntity(repository.findById(id).orElse(null));
+        return mapper.fromEntity(
+                repository.findById(id)
+                        .orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado. ID: " + id + " Tipo: " + UserResponse.class.getSimpleName()))
+        );
     }
 }
