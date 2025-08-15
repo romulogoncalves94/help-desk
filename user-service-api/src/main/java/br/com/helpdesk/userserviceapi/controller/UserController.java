9aa2d4fad2f954a93ad381f8720818783698654a
romulogoncalves94 <romulogoncalves94@hotmail.com>
2025-08-15 10:39:49 -0300

    docs: add OpenAPI docs

--- user-service-api/src/main/java/br/com/helpdesk/userserviceapi/controller/UserController.java
+++ user-service-api/src/main/java/br/com/helpdesk/userserviceapi/controller/UserController.java
@@ -1,13 +1,36 @@
 package br.com.helpdesk.userserviceapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import models.exceptions.StandardError;
 import models.responses.UserResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "UserController", description = "Controller responsável por gerenciar usuários.")
 @RequestMapping("api/users")
 public interface UserController {
 
+    @Operation(summary = "Buscar usuário por ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário encontrado"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = StandardError.class)
+            )),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = StandardError.class)
+            ))
+    })
     @GetMapping("/{id}")
-    ResponseEntity<UserResponse> findById(@PathVariable(name = "id") final String id);
+    ResponseEntity<UserResponse> findById(
+            @Parameter(description = "User ID", required = true, example = "12345")
+            @PathVariable(name = "id") final String id);
 
 }
