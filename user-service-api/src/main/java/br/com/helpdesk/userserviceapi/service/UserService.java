861271105cc79b25432f570df8fbb8289a3e2b3c
romulogoncalves94 <romulogoncalves94@hotmail.com>
2025-08-19 13:39:46 -0300

    feat: add encryptor para senhas dos usuários

--- user-service-api/src/main/java/br/com/helpdesk/userserviceapi/service/UserService.java
+++ user-service-api/src/main/java/br/com/helpdesk/userserviceapi/service/UserService.java
@@ -9,9 +9,12 @@ import models.requests.CreateUserRequest;
 import models.requests.UpdateUserRequest;
 import models.responses.UserResponse;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+import static java.util.Objects.nonNull;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +22,7 @@ public class UserService {
 
     private final UserRepository repository;
     private final UserMapper mapper;
+    private final BCryptPasswordEncoder encoder;
 
     public UserResponse findById(final String id) {
         return mapper.fromEntity(find(id));
@@ -26,7 +30,10 @@ public class UserService {
 
     public void save(CreateUserRequest request) {
         verifyIfEmailAlreadyExists(request.email(), null);
-        repository.save(mapper.fromRequest(request));
+        repository.save(
+                mapper.fromRequest(request)
+                        .withPassword(encoder.encode(request.password()))
+        );
     }
 
     public void verifyIfEmailAlreadyExists(final String email, final String id) {
@@ -47,7 +54,11 @@ public class UserService {
     public UserResponse update(final String id, final UpdateUserRequest request) {
         var entity = find(id);
         verifyIfEmailAlreadyExists(request.email(), id);
-        return mapper.fromEntity(repository.save(mapper.update(request, entity)));
+        return mapper.fromEntity(
+                repository.save(
+                        mapper.update(request, entity)
+                                .withPassword(nonNull(request.password()) ? encoder.encode(request.password()) : entity.getPassword())
+                ));
     }
 
     private User find(final String id) {
