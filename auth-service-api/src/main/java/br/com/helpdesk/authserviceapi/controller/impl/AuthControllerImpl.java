5cdf8ab851c58b15232afe485ada1329476b95a5
romulogoncalves94 <romulogoncalves94@hotmail.com>
2025-09-04 10:07:49 -0300

    feat: add configuração de segurança

---
+++ auth-service-api/src/main/java/br/com/helpdesk/authserviceapi/controller/impl/AuthControllerImpl.java
@@ -0,0 +1,17 @@
+package br.com.helpdesk.authserviceapi.controller.impl;
+
+import br.com.helpdesk.authserviceapi.controller.AuthController;
+import models.requests.AuthenticateRequest;
+import models.responses.AuthenticationResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthControllerImpl implements AuthController {
+
+    @Override
+    public ResponseEntity<AuthenticationResponse> authenticate(AuthenticateRequest request) {
+        return null;
+    }
+
+}
