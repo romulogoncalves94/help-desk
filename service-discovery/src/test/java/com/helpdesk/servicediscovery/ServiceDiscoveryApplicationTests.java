9fff3cf1745f898874d7ca4dc81daab51869b7c3
romulogoncalves94 <romulogoncalves94@hotmail.com>
2026-07-17 09:40:43 -0300

    feat: criado service dicovery

---
+++ service-discovery/src/test/java/com/helpdesk/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpdesk.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
