fbd769c6ee15f6f0b89924de62cc0bcd9e9cc546
romulogoncalves94 <romulogoncalves94@hotmail.com>
2026-07-22 10:46:55 -0300

    feat: criado componente de email-service

---
+++ email-service/src/test/java/com/helpdesk/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpdesk.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
