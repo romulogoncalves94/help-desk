b5ed0775202e90e6d0460c1e3fb024cf3e11e0b3
romulogoncalves94 <romulogoncalves94@hotmail.com>
2026-07-28 08:48:51 -0300

    feat: configurado cache distribuído com Redis para UserService e OrderService.

--- helpdesk-bff/src/main/java/br/com/helpdesk/helpdeskbff/service/OrderService.java
+++ helpdesk-bff/src/main/java/br/com/helpdesk/helpdeskbff/service/OrderService.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import models.requests.CreatedOrderRequest;
 import models.requests.UpdateOrderRequest;
 import models.responses.OrderResponse;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
@@ -16,26 +18,32 @@ public class OrderService {
 
     private final OrderFeignClient client;
 
+    @CacheEvict(value = "orders", allEntries = true)
     public void save(CreatedOrderRequest request) {
         client.save(request);
     }
 
+    @CacheEvict(value = "orders", allEntries = true)
     public OrderResponse update(Long id, UpdateOrderRequest request) {
         return client.update(id, request).getBody();
     }
 
+    @Cacheable(value = "orders", key = "#id")
     public OrderResponse findById(Long id) {
         return client.findById(id).getBody();
     }
 
+    @CacheEvict(value = "orders", allEntries = true)
     public void deleteById(Long id) {
         client.deleteById(id);
     }
 
+    @Cacheable(value = "orders")
     public List<OrderResponse> findAll() {
         return client.findAll().getBody();
     }
 
+    @Cacheable(value = "orders")
     public Page<OrderResponse> findAllPaginated(Integer page, Integer linesPerPage, String direction, String orderBy) {
         return client.findAllPaginated(page, linesPerPage, direction, orderBy).getBody();
     }
--- helpdesk-bff/src/main/java/br/com/helpdesk/helpdeskbff/service/UserService.java
+++ helpdesk-bff/src/main/java/br/com/helpdesk/helpdeskbff/service/UserService.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import models.requests.CreateUserRequest;
 import models.requests.UpdateUserRequest;
 import models.responses.UserResponse;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,18 +17,22 @@ public class UserService {
 
     private final UserFeignClient client;
 
+    @Cacheable(value = "users", key = "#id")
     public UserResponse findById(final String id) {
         return client.findById(id).getBody();
     }
 
+    @CacheEvict(value = "users", allEntries = true)
     public void save(CreateUserRequest request) {
         client.save(request);
     }
 
+    @Cacheable(value = "users")
     public List<UserResponse> findAll() {
         return client.findAll().getBody();
     }
 
+    @CacheEvict(value = "users", allEntries = true)
     public UserResponse update(final String id, final UpdateUserRequest request) {
         return client.update(id, request).getBody();
     }
